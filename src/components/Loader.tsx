85f91e6ae524aabad4ff8fb8971856195e813698
Manji Coder <manjicoder@gmail.com>
2024-10-20 12:40:38 +0530

    Clear all files from main

--- src/components/Loader.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loader() {
-  return (
-    <main className='w-full h-screen flex justify-center items-center'>
-      <div className='spinner'></div>
-    </main>
-  );
-}
--- src/pages/Records.tsx
+++
@@ -1,9 +0,0 @@
-import PageWrapper from '@/components/layout/PageWrapper';
-
-export default function Records() {
-  return (
-    <PageWrapper>
-      <h1 className='text-center'>Records</h1>
-    </PageWrapper>
-  );
-}
