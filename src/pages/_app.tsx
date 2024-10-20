cec9bd37089f7175d467589bf84d26822bdaee75
Manji Coder <manjicoder@gmail.com>
2024-10-20 14:36:02 +0530

    pdfjs-dist working

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import DynamicHead from '@/components/DynamicHead';
+import { ModeToggle } from '@/components/theme/ModeToggle';
+import { ThemeProvider } from '@/components/theme/ThemeProvider';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Rubik } from 'next/font/google';
+
+const inter = Rubik({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={inter.className}>
+      <DynamicHead />
+      <ThemeProvider
+        attribute='class'
+        defaultTheme='system'
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Component {...pageProps} />
+        <ModeToggle />
+      </ThemeProvider>
+    </div>
+  );
 }
