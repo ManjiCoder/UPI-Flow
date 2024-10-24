f8b0fabb2ddbd93e50629320e870d68e98420fdc
Manji Coder <manjicoder@gmail.com>
2024-10-24 22:02:26 +0530

    Lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import AppContainer from './components/AppContainer';
-import Analytics from './pages/Analytics';
-import Home from './pages/Home';
-import Records from './pages/Records';
-import Test from './pages/Test';
+import Loader from './components/Loader';
+// import Analytics from './pages/Analytics';
+// import Home from './pages/Home';
+// import Records from './pages/Records';
+// import Test from './pages/Test';
+
+const Home = lazy(() => import('./pages/Home'));
+const Records = lazy(() => import('./pages/Records'));
+const Analytics = lazy(() => import('./pages/Analytics'));
+const Test = lazy(() => import('./pages/Test'));
 
 export default function App() {
   return (
     <AppContainer>
       <Routes>
-        <Route path='/' Component={Home} />
-        <Route path='/records' Component={Records} />
-        <Route path='/analytics' Component={Analytics} />
-        <Route path='/settings' Component={Test} />
+        <Suspense fallback={<Loader />}>
+          <Route path='/' Component={Home} />
+          <Route path='/records' Component={Records} />
+          <Route path='/analytics' Component={Analytics} />
+          <Route path='/settings' Component={Test} />
+        </Suspense>
       </Routes>
     </AppContainer>
   );
