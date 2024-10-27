46c639dd3ac061edda37c293117b609de9f49807
Manji Coder <manjicoder@gmail.com>
2024-10-27 16:46:51 +0530

    Loader updated

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,6 +1,6 @@
 export default function Loader() {
   return (
-    <main className='w-full h-screen flex justify-center items-center'>
+    <main className='w-full min-h-[90vh] lg:h-screen flex justify-center items-center'>
       <div className='spinner text-primary'></div>
     </main>
   );
